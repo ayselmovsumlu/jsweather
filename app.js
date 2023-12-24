fdde80887c6b84e68dc16ad4e6396a5a70cd7454
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-24 14:30:30 +0000

    weather

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 "use strict";
 $(document).ready(function () {
 
-   
+    $(document).on('keypress', function (e) {
+        if (e.which == 13) {
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$("#myweather").val()}&appid=a1a99039e98fe3334da315311c12b501`
+            async function myweather() {
+                let responce = await fetch(url)
+                let data = await responce.json()
+            }
+            myweather()
 
+        }
+
+    });
+
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${$("#myweather").val()}&appid=a1a99039e98fe3334da315311c12b501`
+        async function myweather() {
+            let responce = await fetch(url)
+            let data = await responce.json()
+        }
+        myweather()
+
+
+
+    });
 
 
 });
