e1d9898c9aa94a5b8e98899bb19c497061782723
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-25 17:51:13 +0000

    function

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 
     $(document).on('keypress', function (e) {
         if (e.which == 13) {
-            let url = 
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=a1a99039e98fe3334da315311c12b501`
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
                 $("#city").html(data.name)
                 $("#temp").html(data.main.temp)
                 $("#feeels").html(data.main.feels_like)
-                $("#myweather").val("");
+                $("#myweather").val("")
                 console.log(data);
             }
             myweather()
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
     $('#btn').click(function (e) {
         e.preventDefault();
-        let url = 
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${$("#myweather").val()}&appid=a1a99039e98fe3334da315311c12b501`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
